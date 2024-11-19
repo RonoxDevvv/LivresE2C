576eb3157d6fbffa27bed7e177647fdd01637a90
RonoxDevvv <enzol.e2c@gmail.com>
2024-11-19 15:29:20 +0100

    JS Start

---
+++ script.js
@@ -0,0 +1,15 @@
+let userName = "delagx_"
+let age = "17"
+let size = "1m75"
+let isCool = true
+
+console.log(userName, age, size, isCool)
+
+isCool = false
+
+console.log(isCool)
+
+let a = 65
+let b = 876
+
+console.log(a*b, a+b, a-b, a%b, a/b)
